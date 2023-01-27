3b3a28eedb92b0bf9533af40163922dd2d00daf9
Luyanda Siko <sikoluyanda@gmail.com>
2023-01-27 16:24:56 +0200

    Added a new feature for Dependency Injection Extension support across the board

---
+++ src/Component/Component.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Xylene\Component;
+
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+
+/**
+ * Interface Component
+ *
+ * @package Xylene\Component
+ * @author Luyanda Siko <sikoluyanda@gmail.com>
+ */
+interface Component extends ExtensionInterface
+{
+}
