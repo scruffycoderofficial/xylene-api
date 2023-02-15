6a522f214678ea2722d9244607b427825c514de9
Luyanda Siko <sikoluyanda@gmail.com>
2023-02-15 10:52:52 +0200

    Added evvent subscriber classes to subscribe to the targeted Kernel Event

---
+++ src/EventSubscriber/Parameter/RequiredRequestParameterException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Xylene\EventSubscriber\Parameter;
+
+use Exception;
+
+/**
+ * Class RequiredRequestParameterException
+ *
+ * @package Xylene\Exception
+ */
+class RequiredRequestParameterException extends Exception
+{
+}
