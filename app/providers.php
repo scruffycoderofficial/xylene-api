86d399aed7644de935e8b5bce68b3975d5af62a7
Luyanda Siko <sikoluyanda@gmail.com>
2023-03-01 17:37:20 +0200

    Removed all App directory related files to provision for Front-end Application

--- app/providers.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Xylene\Provider\DoctrineOrmServiceProvider;
-
-$app->register(new DoctrineOrmServiceProvider());
--- app/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-/**
- * List all Parties on record
- */
-$app->map('/api/parties', "Xylene\Demo\Party\ShowParties::show");
